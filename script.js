fd4d470bd863977629661ffbe2cc3880c5835021
Frank Gines <frnkgines@gmail.com>
2024-04-04 21:03:31 +0800

    Log In Form To Main Form

---
+++ script.js
@@ -0,0 +1,12 @@
+
+//eto yung id ng code inputbox
+const entercode = document.getElementById("Codes");
+
+//so dinagdagan natin ng eventlistener para may gagawin sya
+//once priness natin yung enter Key sa laptop or sa cp keyboard
+//mag proceed na sya sa main html natin
+entercode.addEventListener("keyup", function(event) {
+    if (event.key == "Enter") {
+        window.location.href = "main.html";
+    }
+});
