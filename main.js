8b77d1948d7cf3ca5adfbfc29e9ddd41a3e23efa
Frank Gines <frnkgines@gmail.com>
2024-04-08 10:35:35 +0800

    hiding candidates that are not voted, and adding colors to it

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const candidates = document.getElementById("candidate");
-let button = document.getElementById("btn");
 
 function dynamicallycreateelement(){
     //so this is actually an array objects parang mag sstore lang tayo ng maraming varaible sa array
@@ -11,22 +10,22 @@ function dynamicallycreateelement(){
     ]
 
     //no need ilagay sa object array since lahat naman ng placard gagamit nito
-    let checkimg = "Pictures/GreenCheck.png"
+    let checkimg = "Pictures/GreenCheck.png";
 
     //then of course we need to loop sa object array then sa loob narin ng looping
     //natin iinitialized yung mga dynamically elements as weels yung mga styles nila
     for(let i = 0; i < officers.length; i++){
         let addOfficers = document.createElement('div');
-        addOfficers.classList.add('officers-placard')
+        addOfficers.classList.add('officers-placard');
         //ito namang classlist.add basically parang nag bibigay tayo ng class para magamit natin to sa css
 
         let imgofficer = document.createElement('img');
-        imgofficer.classList.add('imgofficer')
-        imgofficer.src = officers[i].image
+        imgofficer.classList.add('imgofficer');
+        imgofficer.src = officers[i].image;
 
         let imgofficercollege = document.createElement('img');
-        imgofficercollege.classList.add('imgofficercollege')
-        imgofficercollege.src = officers[i].collegeimg
+        imgofficercollege.classList.add('imgofficercollege');
+        imgofficercollege.src = officers[i].collegeimg;
 
         //etong text container as div kasi need natin ilagay dito yung names, quotes as well as yung vote icon
         let textcontainer = document.createElement('div');
@@ -47,20 +46,20 @@ function dynamicallycreateelement(){
         officerquote.textContent = officers[i].quotes;
 
         let votetext = document.createElement('p')
-        votetext.classList.add('votetext')
-        votetext.textContent = "Vote"
-        votetext.style.fontFamily = 'sans-serif'
+        votetext.classList.add('votetext');
+        votetext.textContent = "Vote";
+        votetext.style.fontFamily = 'sans-serif';
 
-        let imgvote = document.createElement('img')
-        imgvote.classList.add('imgvote')
-        imgvote.src = checkimg
+        let imgvote = document.createElement('img');
+        imgvote.classList.add('imgvote');
+        imgvote.src = 'Pictures/GreenCheck.png';
 
-        let votecontainer = document.createElement('div')
-        votecontainer.classList.add('votecontainer')
+        let votecontainer = document.createElement('div');
+        votecontainer.classList.add('votecontainer');
 
         //itong mga nasa baba ibig sabihin naman neto is nilalagay natin sa loob ng element kung ano yung nasa parenthesis
-        votecontainer.appendChild(votetext)
-        votecontainer.appendChild(imgvote)
+        votecontainer.appendChild(votetext);
+        votecontainer.appendChild(imgvote);
 
         textcontainer.appendChild(officername);
         textcontainer.appendChild(officercourse)
@@ -73,6 +72,49 @@ function dynamicallycreateelement(){
 
         //eto yung pinaka mother div yung candidates
         candidates.appendChild(addOfficers);
+
+        //so itong var gagamitin natin para maselect lahat ng elements na officers-placard
+        var allplacards = document.querySelectorAll('.officers-placard');
+        
+        //then mag loloop tayo para lagyan lahat ng elements ng onclick feature
+        //then maglalagay tayo ng default na function() para ma trigger yung nasa lob ng addeventlistener magrarun yon
+        //yung nasa loob kasi non is actually looping ulit icocompare nya yung index ng first loop
+        //sa pangalawang loop, then pag same sila, oh d wala na tayo baguhin
+        //if hinde then hide natin sila
+
+        function showvoted(){
+            allplacards.forEach(function(officer, index) {
+                officer.addEventListener('click', function() {
+                    allplacards.forEach(function(element, idx) {
+                        if (idx == index) {
+                            addOfficers.style.backgroundColor = '#b3ffb3';
+                            imgvote.src = 'Pictures/unvote.png';
+                            votetext.textContent = "UnVote";
+                                
+                            //lagay ulit tayo eventlistener para kapag pinindot ulit ni user yung voted placard ibabalik nya sa dati
+                            officer.addEventListener('click', unhide);
+                            function unhide(){
+                                allplacards.forEach(function(element){
+                                    element.classList.remove('hide');
+                                    addOfficers.style.backgroundColor = 'white';
+                                    imgvote.classList.remove('hide');
+                                    imgvote.src = 'Pictures/GreenCheck.png'
+                                    votetext.classList.remove('hide');
+                                    votetext.textContent = 'Vote';
+                                });
+
+                                showvoted();
+                            }
+                            
+                        } else {
+                            element.classList.add('hide');  //taga natin yung ibang placards
+                        }
+                    });
+                });
+            });
+        }
+
+        showvoted();
     }
 }
 
