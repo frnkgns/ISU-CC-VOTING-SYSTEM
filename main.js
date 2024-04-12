9a78fd283d16416c8e79f7518d776cd16f1a3ccd
Frank Gines <frnkgines@gmail.com>
2024-04-12 13:17:47 +0800

    adding postiongs text

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const president = document.getElementById("president");
 const vicepresident = document.getElementById("vicepresident");
 
 
-function dynamicallycreateelement(){
+function Dynamicallycreateelement(){
     //so this is actually an array objects parang mag sstore lang tayo ng maraming varaible sa array
     const Officers = [
         {position: "president", name:'Frank Ellis A. Gines', collegeimg: 'Pictures/cct.png' ,course: 'BSCS - 3A DM',collegecolor: 'RED',image: 'Pictures/male hap badi.png', quotes: "Life is like riding a bicycle. To keep your balance, you must keep moving."}, 
@@ -12,15 +12,15 @@ function dynamicallycreateelement(){
         {position: "vicepresident", name: 'Mark Paguia', collegeimg: 'Pictures/agri.png', course: 'IAT - 3A DM', collegecolor: 'green', image: 'Pictures/male hap badi.png', quotes: "Life is like riding a bicycle. To keep your balance, you must keep moving." },
         {position: "vicepresident", name: 'Mark Paguia', collegeimg: 'Pictures/agri.png', course: 'IAT - 3A DM', collegecolor: 'green', image: 'Pictures/male hap badi.png', quotes: "Life is like riding a bicycle. To keep your balance, you must keep moving." },
         {position: "vicepresident", name: 'Mark Paguia', collegeimg: 'Pictures/agri.png', course: 'IAT - 3A DM', collegecolor: 'green', image: 'Pictures/male hap badi.png', quotes: "Life is like riding a bicycle. To keep your balance, you must keep moving." }
-
     ]
 
     //no need ilagay sa object array since lahat naman ng placard gagamit nito
     let checkimg = "Pictures/GreenCheck.png";
 
     //then of course we need to loop sa object array then sa loob narin ng looping
-    //natin iinitialized yung mga dynamically elements as weels yung mga styles nila
+    //natin iinitialized yung mga dynamically elements as well as yung mga styles nila
 
+    //dito na starting ng dynamic elements
     for(let i = 0; i < Officers.length; i++){
         let addOfficers = document.createElement('div');
         addOfficers.classList.add(Officers[i].position + "-placard");
@@ -61,6 +61,8 @@ function dynamicallycreateelement(){
         imgvote.classList.add('imgvote');
         imgvote.src = 'Pictures/GreenCheck.png';
 
+        //dito sa votecontainer natin ilalagay yung imagevote, tyaka textvote, para kaapg nag adjust tayo 
+        //sa css container nlang gagalawin natin, 'sana'
         let votecontainer = document.createElement('div');
         votecontainer.classList.add('votecontainer');
 
@@ -77,30 +79,29 @@ function dynamicallycreateelement(){
         addOfficers.appendChild(imgofficercollege);
         addOfficers.appendChild(textcontainer);
 
+        //ilalagay natin sa perfective positions yung mga candidates, hindi kasi pwedeng nasa president category
+        //si treasurer and the rest of the positions, actually maganda to kapag loop
         if(Officers[i].position == 'president'){
             president.appendChild(addOfficers);
 
         } else if(Officers[i].position == 'vicepresident'){
             vicepresident.appendChild(addOfficers);
         }    
-
-
-        //so itong var gagamitin natin para maselect lahat ng elements na officers-placard
                     
-        //then mag loloop tayo para lagyan lahat ng elements ng onclick feature
-        //then maglalagay tayo ng default na function() para ma trigger yung nasa lob ng addeventlistener magrarun yon
-        //yung nasa loob kasi non is actually looping ulit icocompare nya yung index ng first loop
-        //sa pangalawang loop, then pag same sila, oh d wala na tayo baguhin
-        //if hinde then hide natin sila
-
+        //then itong function mag seserved sa lahat ng placards ng click function as well as yung hide functions nila
         function showvoted(){
+            //then itong part needed para makapag loop tayo sa selection ng placards from pres-businessM
+            //katamad kapag mano mano tyaka madaming code masyado
             var Positions = ['president', 'vicepresident'];
 
             Positions.forEach(function(Positions){
                 var allplacards = document.querySelectorAll('.' + Positions + '-placard');
-
+                //then dito na mag start yung loop 
                 allplacards.forEach(function(officer, index) {
+                    //maglalagay tayo ng click function sa mga placards
                     officer.addEventListener('click', function firstclick() {
+                        //then kapag yung pinindot nila yung placard na yun, yung lang yung malalagyan ng effects
+                        //the rest ihahide nanatin
                         allplacards.forEach(function(element, idx) {
                             if (idx == index){
                                 officer.style.backgroundColor = '#b3ffb3';
@@ -109,11 +110,12 @@ function dynamicallycreateelement(){
                                 imgvote.src = 'Pictures/unvote.png';
                                 votetext.textContent = "UnVote";
                                 
-                                //need muna natin iremove yung click function kasi para makapag click tayo later on ulit sa placard
-                                //lagay ulit tayo eventlistener para kapag pinindot ulit ni user yung voted placard ibabalik nya sa dati
+                                //need muna natin iremove yung click function para makapag click tayo later on ulit sa placard
+                                //lagay ulit tayo eventlistener para kapag pinindot ulit ni user yung voted placard ibabalik nya yung unvoted candidates
                                 officer.removeEventListener('click', firstclick);
                                 officer.addEventListener('click', unhide);
                                 function unhide(){
+                                    //then eto sa baba kapag ibabalik sa dati yung mga componetns ng ibang placard
                                     allplacards.forEach(function(element){
                                         element.classList.remove('hide');
                                         officer.style.backgroundColor = 'white';
@@ -122,7 +124,8 @@ function dynamicallycreateelement(){
                                         votetext.classList.remove('hide');
                                         votetext.textContent = 'Vote';
                                     });    
-                                    //then iadd ulit natin yung vote function
+
+                                    //then iadd ulit natin yung vote function, para makapag click tayo since niremove natin kanina
                                     officer.addEventListener('click', firstclick);
                                 }
                                 
@@ -135,11 +138,13 @@ function dynamicallycreateelement(){
             })
         }
 
-        //initialized muna natin yung function
+        //of course hindi gagana yung function kapag hindi mo natawag, kaya need muna natin tawagin
         showvoted();
     }
 
+    //pati to syempte
     DisplayOfficer(Officers);
 }
 
-dynamicallycreateelement();
+//lalo nato
+Dynamicallycreateelement();
